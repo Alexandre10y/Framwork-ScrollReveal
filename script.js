6059816e57151447009659d02f6125bbc1431e16
Alexandre Belloni <86206091+Alexandre10y@users.noreply.github.com>
2024-05-24 19:47:38 -0300

    Carregando Primeira versao
    Index.html
    Style.css
    Script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+window.sr = ScrollReveal({
+	reset:true 
+});
+
+sr.reveal('.area-1',{	
+	duration: 2000,
+	rotate: {x:80, y:80, z:0}
+})
+sr.reveal('.area-2',{	
+	duration: 500,
+	rotate: {x:100, y:100, z:0}
+})
+sr.reveal('.area-3',{	
+	duration: 2000,
+	rotate: {x:100, y:0, z:0}
+})
